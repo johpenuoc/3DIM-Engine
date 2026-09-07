253cff6a10a8f76510543b4e6ec3fd4153c00373
John <325361506+johpenuoc@users.noreply.github.com>
2026-09-08 00:13:11 +0100

    A few fixes

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -7,17 +7,19 @@ class Engine:
         self.main = Main
         self.camera = Camera()
 
+        self.rays = self.camera.collect_rays()
+
     # not really tracing, i just didnt know what to call it
     def trace_rays(self):
         #rays = list(self.camera.collect_rays().values())
-        rays = self.camera.collect_rays()
         ppos = self.main.p.pos
-        print(ppos)
+        #print(ppos)
 
-        for transform in rays['0']:
+        for transform in self.rays['0'][:int(len(self.rays['0']) / 4)]:
             # these are the end points of each ray; relative to the player
             x = ppos[0] - transform[0]
             y = ppos[1] - transform[1]
             z = ppos[2] - transform[2]
+            #print('y pos: ', y)
 
-            pygame.draw.line(self.main.display, (255, 0, 0), ppos[:2], (ppos[0] - x, ppos[1] - y))
+            pygame.draw.line(self.main.display, (255, 0, 0), ppos[:2], (x, y))
